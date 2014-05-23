6c12ee3dee8b45f2546bb2412e66f4a9eb8920a6
Matt Windsor <mbw500@york.ac.uk>
2014-05-23 16:17:53 +0100

    Remove example code, add Unix interface.

--- lib/bikesched.rb
+++ lib/bikesched.rb
@@ -7,11 +7,8 @@ require 'time'
 require 'English'
 
 module Bikesched
-  def self.database_schedule
+  def self.schedule
     sd = ScheduleDatabase.from_dbpasswd
     Schedule.new(sd)
   end
-
-  shows = self.database_schedule.from(Time.now).for(1).week
-  UnixOutputter.new.output_schedule_slice(shows)
 end
