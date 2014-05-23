51f7573c577e1e133b71d03eae267f367dc49f12
mattbw <matt.windsor@ury.york.ac.uk>
2014-05-23 14:59:08 +0100

    Factor out outputter.

--- lib/bikesched.rb
+++ lib/bikesched.rb
@@ -1,3 +1,4 @@
+require 'bikesched/unix_outputter'
 require 'bikesched/version'
 require 'bikesched/schedule'
 require 'bikesched/schedule_database'
@@ -12,20 +13,5 @@ module Bikesched
   end
 
   shows = self.database_schedule.from(Time.now).for(1).week
-
-  $OFS = ':'
-  $ORS = "\n"
-
-  def self.escape(string)
-    string.gsub(/([#{$OFS}#{$ORS}])/, '\\\\\1')
-  end
-
-  shows.each do |show|
-    print(show[:show_id],
-          show[:show_season_id],
-          show[:show_season_timeslot_id],
-          show[:start_time].to_i,
-          show[:end_time].to_i,
-          escape(show[:show_name]))
-  end
+  UnixOutputter.new.output_schedule_slice(shows)
 end
---
+++ lib/bikesched/unix_outputter.rb
@@ -0,0 +1,37 @@
+module Bikesched
+  class UnixOutputter
+    def initialize(handle=nil, record_separator=nil, field_separator=nil)
+      @handle           = handle           || $stdout
+      @record_separator = record_separator || "\n"
+      @field_separator  = field_separator  || ':'
+    end
+
+    def output_schedule_slice(shows)
+      with_separators { shows.each(&method(:schedule_entry)) }
+    end
+
+    private
+
+    def with_separators
+      @old_OFS, @old_ORS = $OFS, $ORS
+      $OFS, $ORS = @field_separator, @record_separator
+
+      yield
+
+      $OFS, $ORS = @old_OFS, @old_ORS
+    end
+
+    def escape(string)
+      string.gsub(/([#{$OFS}#{$ORS}])/, '\\\\\1')
+    end
+
+    def schedule_entry(show)
+      @handle.print(show[:show_id],
+                    show[:show_season_id],
+                    show[:show_season_timeslot_id],
+                    show[:start_time].to_i,
+                    show[:end_time].to_i,
+                    escape(show[:show_name]))
+    end
+  end
+end
