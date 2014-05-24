0866f3128195c31fc8d956827efcc96f4babeb5f
mattbw <matt.windsor@ury.york.ac.uk>
2014-05-25 00:44:06 +0100

    Correct and generalise filler.
    It can now be used directly as the engine for `fill-slice`.

--- lib/bikesched/filler.rb
+++ lib/bikesched/filler.rb
@@ -1,24 +1,39 @@
+require 'forwardable'
+
 module Bikesched
   class Filler
-    def initialize(name='Jukebox', filler_entry_proc=method(:default_fill))
-      @name              = name
-      @filler_entry_proc = filler_entry_proc
+    extend Forwardable
+
+    def initialize(args={})
+      @name         = args[:name]        || 'Jukebox'
+      @fill_proc    = args[:fill_proc]   || method(:default_fill)
+      @start_proc   = args[:start_proc]  || ->(show) { show[:start_time] }
+      @finish_proc  = args[:finish_proc] || ->(show) { show[:finish_time] }
     end
 
     # Fills a list of slots
     #
-    # Each slot will be yielded 
-    def fill(slots, &block=method(:ignore))
-      to_lazy_pairs(slots).flat_map(&method(:try_fill_pair)).each(&block).force
+    # Each slot after filling will be yielded to the block.
+    def fill(slots, &block)
+      @just_started = true
+      to_lazy_pairs(slots.lazy).flat_map(&method(:try_fill_pair)).each(&block)
     end
 
     private
 
     # Tries filling between a pair if necessary
     def try_fill_pair(pair)
-      result = [pair.first]
-      result << fill_pair(pair) if gap?(*&pair)
+      result = []
+      result << pair.first       if @just_started
+      @just_started = false
+
+      result << fill_pair(*pair) if gap?(*pair)
+      result << pair.last
       result
+    end
+
+    def fill_pair(this_slot, next_slot)
+      fill_between(finish_of(this_slot), start_of(next_slot))
     end
 
     # Converts a list of slots to a lazy enumerator of slot pairs
@@ -26,12 +41,12 @@ module Bikesched
     # For a list of slots [a, b, c, ..., z] this will enumerate
     # [[a, b], [b, c], [c, d], ..., [z, nil]].
     def to_lazy_pairs(slots)
-      slots.lazy.zip(slots.lazy.drop(1))
+      slots.each_cons(2)
     end
 
     # Decides whether there is a gap between two slots
     def gap?(this_slot, next_slot)
-      !(next_slot.nil? || next_slot.start_time == this_slot.finish_time)
+      !(next_slot.nil? || finish_of(this_slot) == start_of(next_slot))
     end
 
     def default_fill(start, finish)
@@ -45,5 +60,9 @@ module Bikesched
 
     def ignore(*_)
     end
+
+    def_delegator :@fill_proc,   :call, :fill_between
+    def_delegator :@finish_proc, :call, :finish_of
+    def_delegator :@start_proc,  :call, :start_of
   end
 end
